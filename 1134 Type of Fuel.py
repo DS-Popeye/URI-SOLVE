28a20ae03355c589d423ed1020d90c1c84d8f2e3
Shuvo-70 <rabiul35-487@diu.edu.bd>
2021-11-06 01:52:51 +0600

    Easy.

--- 1134 Type of Fuel.py
+++ 1134 Type of Fuel.py
@@ -1,17 +1,20 @@
-a = 0
-b = 0
-c = 0
+x=0
+y=0
+z=0
 while(True):
-    n = int(input())
-    if(n == 4):
+    a=int(input())
+    if(a == 4):
         break
-    elif(n == 1):
-        a += 1
-    elif(n == 2):
-        b += 1
-    elif(n == 3):
-        c += 1
+    else:
+        if (a == 1):
+            x +=1
+        elif (a == 2):
+            y+=1
+        elif (a == 3):
+            z+=1
+        else:
+            continue
 print("MUITO OBRIGADO")
-print("Alcool:",a)
-print("Gasolina:",b)
-print("Diesel: ",c)
+print("Alcool:",x)
+print("Gasolina:",y)
+print("Diesel:",z)
