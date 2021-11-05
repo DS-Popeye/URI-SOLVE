4c1758b9392d680161bf9381cda28835d4184b42
Shuvo-70 <rabiul35-487@diu.edu.bd>
2021-11-05 23:14:57 +0600

    Some kind of Dificult

--- 1118 Several Scores with Validation.py
+++ 1118 Several Scores with Validation.py
@@ -8,12 +8,15 @@ while True:
             q += 1
         else:
             print("nota invalida")
+
     print("media = %.2f" % (s / 2))
     t = 0
+
     while True:
         print("novo calculo (1-sim 2-nao)")
         t= int(input())
         if (t == 1 or t== 2):
             break
+
     if (t == 2):
         break
