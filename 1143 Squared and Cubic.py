d2fe41c5192cbe19eed5f19b0cfdf146394f25ce
Shuvo-70 <rabiul35-487@diu.edu.bd>
2021-11-06 22:56:21 +0600

    It's Difficult for First time.😊

--- 1143 Squared and Cubic.py
+++ 1143 Squared and Cubic.py
@@ -1,5 +1,10 @@
 while (True):
     try:
+        n = int(input())
+        a,b,c = 1,1,1
+        for i in range(n):
+            print('{0} {1} {2}'.format(a,b,c))
+
 
     except EOFError:
         break
