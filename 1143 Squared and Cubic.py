f8205d7063e175e695a39e83d7dc2a152d4d2a84
Shuvo-70 <rabiul35-487@diu.edu.bd>
2021-11-06 22:59:17 +0600

    It's Difficult for First time.😊

--- 1143 Squared and Cubic.py
+++ 1143 Squared and Cubic.py
@@ -4,7 +4,8 @@ while (True):
         a,b,c = 1,1,1
         for i in range(n):
             print('{0} {1} {2}'.format(a,b,c))
-
-
+            a = a+1
+            b = a*a
+            c = a*b
     except EOFError:
         break
